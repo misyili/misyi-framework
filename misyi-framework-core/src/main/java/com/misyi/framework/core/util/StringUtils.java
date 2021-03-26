d15c3bbf3d09da63f94b161fd71ea141df59b18a
李聪 <296114711@qq.com>
2021-03-26 11:22:13 +0800

    StringUtils 新增下划线和驼峰互转方法

--- misyi-framework-core/src/main/java/com/misyi/framework/core/util/StringUtils.java
+++ misyi-framework-core/src/main/java/com/misyi/framework/core/util/StringUtils.java
@@ -23,5 +23,56 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
         return UUID.randomUUID().toString().replaceAll("-", "");
     }
 
+    /**
+     * 下划线转驼峰
+     * user_name  ---->  userName
+     * house.user_name  ---->  userName
+     * userName   --->  userName
+     *
+     * @param underlineName 带有下划线的名字
+     * @return 驼峰字符串
+     */
+    public static String underlineToHump(String underlineName) {
+        //截取下划线分成数组
+        char[] charArray = underlineName.toCharArray();
+        //判断上次循环的字符是否是"_"
+        boolean underlineBefore = false;
+        StringBuilder buffer = new StringBuilder();
+        for (int i = 0, l = charArray.length; i < l; i++) {
+            //判断当前字符是否是"_",如果跳出本次循环
+            if (charArray[i] == 95) {
+                underlineBefore = true;
+            } else if (underlineBefore) {
+                //如果为true，代表上次的字符是"_",当前字符需要转成大写
+                buffer.append(charArray[i] -= 32);
+                underlineBefore = false;
+            } else { //不是"_"后的字符就直接追加
+                buffer.append(charArray[i]);
+            }
+        }
+        return buffer.toString();
+    }
 
+    /**
+     * 驼峰转 下划线
+     * userName  ---->  user_name
+     * user_name  ---->  user_name
+     *
+     * @param humpName 驼峰命名
+     * @return 带下滑线的String
+     */
+    public static String humpToUnderline(String humpName) {
+        //截取下划线分成数组，
+        char[] charArray = humpName.toCharArray();
+        StringBuilder buffer = new StringBuilder();
+        //处理字符串
+        for (int i = 0, l = charArray.length; i < l; i++) {
+            if (charArray[i] >= 65 && charArray[i] <= 90) {
+                buffer.append("_").append(charArray[i] += 32);
+            } else {
+                buffer.append(charArray[i]);
+            }
+        }
+        return buffer.toString();
+    }
 }
